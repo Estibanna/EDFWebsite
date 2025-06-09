ba9af54b630560c278a1fe5fd4f3034e62db1e50
Estibanna <estibannatwitch@gmail.com>
2025-06-09 21:52:41 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ def callback():
 def login():
     discord_login_url = (
         "https://discord.com/api/oauth2/authorize"
-        "?client_id=JOUW_CLIENT_ID"
+        "?client_id=1381635320658788363"
         "&redirect_uri=https%3A%2F%2Fedfwebsite.onrender.com%2Fcallback"
         "&response_type=code"
         "&scope=identify%20guilds"
